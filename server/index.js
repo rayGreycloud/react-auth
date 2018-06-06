7155d41da34982e631f13edd2f09e5be10aca66c
rayGreycloud <rayGreycloud@gmail.com>
2018-06-06 11:38:48 -0700

    Add router and connect mongoose

--- server/index.js
+++ server/index.js
@@ -2,11 +2,19 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const router = require('./router');
+
+// DB setup
+mongoose.connect('mongodb://localhost:27017/auth');
 
 // App Setup
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+
+router(app);
 
 // Server Setup
 const port = process.env.PORT || 3000;
