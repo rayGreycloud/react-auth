8f67c51c471a657b1e6cd12c9559ee40c6892c2b
rayGreycloud <rayGreycloud@gmail.com>
2018-06-06 15:45:54 -0700

    Add pre middleware to hash password before saved

--- server/models/User.js
+++ server/models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt-nodejs');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -7,8 +8,29 @@ const userSchema = new Schema({
   password: String
 });
 
+// On save hook, encrypt password
+userSchema.pre('save', function(next) {
+  // Get access to user model
+  const user = this;
+  // Generate salt
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+    // Hash password
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+      // Set db password to hashed password
+      user.password = hash;
+      next();
+    });
+  });
+});
+
 // Create a model class
-const ModelClass = mongoose.model('user', UserSchema);
+const ModelClass = mongoose.model('user', userSchema);
 
 // Export the model
 module.exports = ModelClass;
