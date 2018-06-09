ea5c06b98fcf75ee3fb0bf318f6f01608880bb7b
rayGreycloud <rayGreycloud@gmail.com>
2018-06-09 16:37:13 -0700

    Change server port to avoid conflict with cra dev server

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
 // Server Setup
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 server.listen(port);
 console.log(`Listening on port ${port}...`);
