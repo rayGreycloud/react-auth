d1a47dc7de152281f67f7c27a051569f544e2a46
rayGreycloud <rayGreycloud@gmail.com>
2018-06-10 07:57:34 -0700

    Set up redux form; add fields for email and password

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
 
 class Signup extends Component {
   render() {
     return (
       <form>
         <fieldset>
-          <label>Email</label>
+          <label>Email </label>
+          <Field
+            name="email"
+            type="text"
+            component="input"
+            autoComplete="none"
+            placeholder=" enter your email"
+          />
         </fieldset>
         <fieldset>
-          <label>Password</label>
+          <label>Password </label>
+          <Field
+            name="password"
+            type="password"
+            component="input"
+            autoComplete="none"
+            placeholder=" enter a password"
+          />
         </fieldset>
       </form>
     );
   }
 }
 
-export default Signup;
+export default reduxForm({ form: 'signup' })(Signup);
