f0de42fb3c028616cf41c580e4977ccd0179047c
rayGreycloud <rayGreycloud@gmail.com>
2018-06-10 07:56:41 -0700

    Create redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducers from './reducers';
 import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
 
+const store = createStore(reducers, {});
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Route path="/" exact component={Welcome} />
-      <Route path="/signup" exact component={Signup} />
-    </App>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App>
+        <Route path="/" exact component={Welcome} />
+        <Route path="/signup" exact component={Signup} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
   document.querySelector('#root')
 );
