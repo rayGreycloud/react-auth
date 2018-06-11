bd1afaf441f87f6b744b6452e678f2084a3b3328
rayGreycloud <rayGreycloud@gmail.com>
2018-06-11 06:27:22 -0700

    Add signout and feature components; Add redux thunk to store; Set default auth state by checking LS for jwt

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
 import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
+import Signout from './components/auth/Signout';
+import Feature from './components/Feature';
 
-const store = createStore(reducers, {});
+const store = createStore(
+  reducers,
+  {
+    auth: { authenticated: localStorage.getItem('token') }
+  },
+  applyMiddleware(reduxThunk)
+);
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App>
         <Route path="/" exact component={Welcome} />
-        <Route path="/signup" exact component={Signup} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/signout" component={Signout} />
+        <Route path="/feature" component={Feature} />
       </App>
     </BrowserRouter>
   </Provider>,
